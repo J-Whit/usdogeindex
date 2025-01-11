d48805563bd0ad66a4adf0c16698cf15e3d36933
Jared Whitney <jpwhitney8@gmail.com>
2025-01-11 18:47:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ const usdChart = new Chart(ctx, {
         labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
         datasets: [
             {
-                label: 'USDOGE Strength',
-                data: [50, 55, 60, 58, 62, 65, 70],
+                label: 'DOGE Strength',
+                data: [9000, 100000, 500000, 450000, 777777, 999999, 10000000],
                 borderColor: '#28a745',
                 backgroundColor: 'rgba(40, 167, 69, 0.2)',
                 borderWidth: 2,
